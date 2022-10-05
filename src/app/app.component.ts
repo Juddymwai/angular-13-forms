31f72f2f352a9bf9a906cc2d7bc34b22c828ee01
Judy Mwai <juddiemukami@gmail.com>
2022-10-05 12:51:02 +0300

    adds validators to form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl,FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+ import Validation from './utils/Validation';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-13-form';
+export class AppComponent implements OnInit{
+  form: FormGroup = new FormGroup({
+    fullname: new FormControl(''),
+    username: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+    confirmPassword: new FormControl(''),
+    acceptTerms: new FormControl('false'),
+    
+
+  });
+  submitted =false;
+
+  constructor(private formBuilder: FormBuilder) {}
+  ngOnInit(): void {
+    this.form =this.formBuilder.group(
+      {
+        fullname: ['', Validators.required],
+        username: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(20)
+
+          ]
+        ],
+        email: ['',[Validators.required, Validators.email]],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(40)
+
+          ]
+        ],
+        confirmPassword: ['', Validators.required],
+        acceptTerms: [false,Validators.requiredTrue]
+      },
+      {
+        validators: [Validation.match('password','confirmPassword')]
+      }
+
+    );
+  }
+  // ... 
+
+  
+
+
 }
